7e4fd093859936097adae9ea00dcc494dbbd4eef
Jake Meyer <jacob.robert.meyer@gmail.com>
2014-03-13 16:52:16 -0600

    added form animations

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -10,21 +10,40 @@ $(function(){
 
 	$(document).on('click', '.next', function(e){
 		e.preventDefault();
-		var thisForm = $(this).closest('.form-page')
-		thisForm.hide();
-		var nextFormData = parseInt(thisForm.attr('data-section')) + 1;
-		$('.form-page[data-section='+nextFormData+']').show();
-		if (nextFormData === 3) {
-			$('.passage-div[data-passage=0]').css('display', 'block');
-		}
+		var thisForm = $(this).closest('.form-page');
+		thisForm.animate({'right':'2000px'}, 400, function(){
+			thisForm.hide()
+			thisForm.css('right','');
+			var nextFormData = parseInt(thisForm.attr('data-section')) + 1;
+			var newForm = $('.form-page[data-section='+nextFormData+']');
+			newForm.css('left','2000px');
+			newForm.show();
+			if (nextFormData === 3) {
+				$('.passage-div[data-passage=0]').css('display', 'block');
+			}
+			newForm.animate({'left':''}, 400, function(){
+				newForm.css('left','');
+			});
+			
+		});
+		
 
 	});
 	$(document).on('click', '.next-passage', function(e){
 		e.preventDefault();
 		var thisPassage = $(this).closest('.passage-div');
-		thisPassage.hide();
-		var nextPassage = parseInt(thisPassage.attr('data-passage')) + 1;
-		$('.passage-div[data-passage='+nextPassage+']').show();
+		thisPassage.animate({'right':'2000px'}, 400, function(){
+			thisPassage.hide();
+			thisPassage.css('right','');
+			var nextPassage = parseInt(thisPassage.attr('data-passage')) + 1;
+			var newPassage = $('.passage-div[data-passage='+nextPassage+']')
+			newPassage.css('left','2000px');
+			newPassage.show();
+			newPassage.animate({'left':''},400, function(){
+				newPassage.css('left','');
+			});
+		})
+		
 		
 	});
 	$(document).on('click', '.prev', function(e){
@@ -34,25 +53,39 @@ $(function(){
 		if (nextFormData === 0) {
 			return false;
 		} else {
-			thisForm.hide();
-			$('.form-page[data-section='+nextFormData+']').show();
+			thisForm.animate({'left':'2000px'}, 400, function(){
+				thisForm.hide()
+				thisForm.css({'left':''})
+				var newForm = $('.form-page[data-section='+nextFormData+']');
+				newForm.css({'right':'2000px'})
+				newForm.show();
+				newForm.animate({'right':''}, 200, function(){
+					newForm.css('right','');
+				});
+			});
 		}
-		
-		
-
 	});
 	$(document).on('click', '.prev-passage', function(e){
 		e.preventDefault();
 		var thisPassage = $(this).closest('.passage-div');
-		thisPassage.hide();
-		var nextPassage = parseInt(thisPassage.attr('data-passage')) - 1;
-		if (nextPassage === -1) {
-			$('.form-page[data-section=2]').show()
-		} else {
-			$('.passage-div[data-passage='+nextPassage+']').show();
-		}
-		
-		
+		thisPassage.animate({'left':'2000px'}, 400, function(){
+			thisPassage.hide();
+			thisPassage.css('left','');
+			var nextPassage = parseInt(thisPassage.attr('data-passage')) - 1;
+			if (nextPassage === -1) {
+				var newForm = $('.form-page[data-section=2]');
+				newForm.css({'right':'2000px'});
+				newForm.show();
+				newForm.animate({'right':''}, 200);
+			} else {
+				var newForm = $('.passage-div[data-passage='+nextPassage+']');
+				newForm.css({'right':'2000px'});
+				newForm.show();
+				newForm.animate({'right':''}, 200, function(){
+					newForm.css('right','');
+				});
+			}
+		});
 	});
 
 });
